8d2b60c0687b4a20f9b8f7410a725b95d7fe8c59
JoãoV <168022398+cleitao-anao@users.noreply.github.com>
2025-03-01 20:15:26 +0000

    mudancas para o servidor funcionar

--- chatbot.js
+++ chatbot.js
@@ -1,17 +1,20 @@
-// leitor de qr code
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const { Client, Buttons, List, MessageMedia } = require('whatsapp-web.js'); // Mudança Buttons
-const client = new Client();
-// serviço de leitura do qr code
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+
+const client = new Client({
+    authStrategy: new LocalAuth() // Salva a sessão localmente
 });
-// apos isso ele diz que foi tudo certo
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
 client.on('ready', () => {
     console.log('Tudo certo! WhatsApp conectado.');
 });
-// E inicializa tudo 
+
 client.initialize();
+
 
 const delay = ms => new Promise(res => setTimeout(res, ms)); // Função que usamos para criar o delay entre uma ação e outra
 
